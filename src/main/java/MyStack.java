a7ca522589064b944a12ec0fcb29749a252fb7c9
Tran Thu Hue <94huetran@gmail.com>
2018-07-05 13:13:48 +0700

    add MyStack

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,49 @@
+public class MyStack {
+    private int size;
+    private int[] arr;
+    private int indext;
+
+    public MyStack() {
+
+    }
+
+    public MyStack(int size) {
+
+        this.size = size;
+        arr = new int[size];
+    }
+
+    public void push(int element) {
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[indext] = element;
+        indext++;
+    }
+
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is null");
+        }
+        return arr[--indext];
+    }
+
+    public int size() {
+        return indext;
+    }
+
+    public boolean isEmpty() {
+        if (indext == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        if (indext == size) {
+            return true;
+        }
+        return false;
+    }
+
+}
